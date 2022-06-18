e2201644c53b6305ad09e7e9866ccf79bcb02784
AllanDutra <allandutrafeitosa14@gmail.com>
2022-06-18 18:21:30 -0300

    feat: node-crud, watched video

---
+++ src/services/CreateVideoService.ts
@@ -0,0 +1,32 @@
+import { FindOneOptions, getRepository } from "typeorm";
+import { Category } from "../entities/Category";
+import { Video } from "../entities/Video";
+
+type VideoRequest = {
+    name: string;
+    description: string;
+    duration: number;
+    category_id: string;
+}
+
+export class CreateVideoService {
+    async execute({ name, description, duration, category_id }: VideoRequest): Promise<Video | Error> {
+        const repo = getRepository(Video); // O CENÁRIO IDEAL SERIA TER ESTES REPOSITÓRIOS EM UMA CAMADA SEPARADA.
+        const repoCategory = getRepository(Category);
+
+        if (!(await repoCategory.findOne(category_id as FindOneOptions<Category>))) {
+            return new Error("Category does not exists!");
+        }
+
+        const video = repo.create({
+            name,
+            description,
+            duration,
+            category_id
+        });
+
+        await repo.save(video);
+
+        return video;
+    }
+}
---
+++ src/services/DeleteCategoryService.ts
@@ -0,0 +1,14 @@
+import { FindOneOptions, getRepository } from "typeorm";
+import { Category } from "../entities/Category";
+
+export class DeleteCategoryService {
+    async execute(id: string) {
+        const repo = getRepository(Category);
+
+        if (!(await repo.findOne(id as FindOneOptions<Category>))) {
+            return new Error("Category does not exists!");
+        }
+
+        await repo.delete(id);
+    }
+}
